ef8a59b8bd3c1f71aa14c832d4b3cfc30b6c5e22
Sean Ogden <positivelyskewed.sf@gmail.com>
2013-05-27 18:34:08 -0400

    added beginnings of master schema.

---
+++ tools/master.py
@@ -0,0 +1,10 @@
+import hyperclient
+c = hyperclient.Client('127.0.0.1', 1900)
+c.add_space('''
+space wtf_master 
+key path_range 
+attributes map(int, string) blocks 
+subspace blocks 
+create 8 partitions
+tolerate 2 failures
+''')
