892badaaef5d83fb275766440e0c39d945a46d35
jl982 <jl982@cornell.edu>
2014-02-21 04:06:05 -0500

    add options for hyperdex host and port, as well as verbose

--- tools/stat.cc
+++ tools/stat.cc
@@ -1,12 +1,16 @@
+//#define __STDC_LIMIT_MACROS
+
 // C/C++
 #include <stdio.h>
 #include <string.h>
 #include <map>
+//#include <stdint.h>
 
 // E
 #include <e/endian.h>
 #include <e/unpacker.h>
 #include <e/intrusive_ptr.h>
+#include <e/popt.h>
 
 // HyperDex
 #include <hyperdex/client.hpp>
@@ -16,9 +20,13 @@
 
 using namespace std;
 
+static long _hyper_port = 1982;
+static const char* _hyper_host = "127.0.0.1";
+static const char* _query = "";
+static bool _verbose = false;
+
 hyperdex::Client* h;
 const char* WTF_SPACE = "wtf";
-bool verbose = true;
 const struct hyperdex_client_attribute* attrs;
 size_t attrs_sz;
 hyperdex_client_returncode status;
@@ -27,11 +35,11 @@ int64_t retval;
 void
 print_return()
 {
-    if (verbose) printf("first retval %ld status %d:", retval, status);
-    if (verbose) cout << status << endl;
+    if (_verbose) printf("first retval %ld status %d:", retval, status);
+    if (_verbose) cout << status << endl;
     retval = h->loop(-1, &status);
-    if (verbose) printf("final retval %ld status %d:", retval, status);
-    if (verbose) cout << status << endl;
+    if (_verbose) printf("final retval %ld status %d:", retval, status);
+    if (_verbose) cout << status << endl;
 }
 
 void
@@ -40,7 +48,7 @@ read()
     if (status == HYPERDEX_CLIENT_SEARCHDONE) return;
 
     // Reading
-    if (verbose) cout << "[" << attrs_sz << "] attributes" << endl;
+    if (_verbose) cout << "[" << attrs_sz << "] attributes" << endl;
     if (attrs == NULL)
     {
         cout << "attrs is NULL" << endl;
@@ -50,11 +58,11 @@ read()
         e::intrusive_ptr<wtf::file> f = new wtf::file("", 0);
         for (size_t i = 0; i < attrs_sz; ++i)
         {
-            if (verbose) cout << i << ". [" << attrs[i].attr << "]:";
+            if (_verbose) cout << i << ". [" << attrs[i].attr << "]:";
             if (strcmp(attrs[i].attr, "path") == 0)
             {
                 string path(attrs[i].value, attrs[i].value_sz);
-                cout << "[" << path << "]" << endl;
+                if (_verbose) cout << "[" << path << "]" << endl;
                 f->path(path.c_str());
             }
             else if (strcmp(attrs[i].attr, "blockmap") == 0)
@@ -62,11 +70,11 @@ read()
                 e::unpacker up(attrs[i].value, attrs[i].value_sz);
                 if (attrs[i].value_sz == 0)
                 {
-                    cout << endl;
+                    if (_verbose) cout << endl;
                 }
                 else
                 {
-                    cout << "[" << up.as_slice().hex() << "]" << endl;
+                    if (_verbose) cout << "[" << up.as_slice().hex() << "]" << endl;
                     up = up >> f;
                 }
             }
@@ -77,7 +85,7 @@ read()
                 e::unpacker up(attrs[i].value, attrs[i].value_sz);
                 up = up >> is_dir;
                 e::unpack64be((uint8_t*)&is_dir, &is_dir);
-                cout << "[" << is_dir << "]" << endl;
+                if (_verbose) cout << "[" << is_dir << "]" << endl;
                 f->is_directory = is_dir == 0 ? false : true;
             }
             else if (strcmp(attrs[i].attr, "mode") == 0)
@@ -87,12 +95,12 @@ read()
                 e::unpacker up(attrs[i].value, attrs[i].value_sz);
                 up = up >> mode;
                 e::unpack64be((uint8_t*)&mode, &mode);
-                cout << "[" << mode << "]" << endl;
+                if (_verbose) cout << "[" << mode << "]" << endl;
                 f->mode = mode;
             }
             else
             {
-                cout << "<unexpected attribute>" << endl;
+                if (_verbose) cout << "<unexpected attribute>" << endl;
             }
         }
         cout << *f << endl;
@@ -102,7 +110,7 @@ read()
 void
 search(const char* attr, const char* value, hyperpredicate predicate)
 {
-    if (verbose) cout << ">>>>searching [" << attr << "] for [" << value << "] with [" << predicate << "]" << endl;
+    cout << ">>>>searching [" << attr << "] for [" << value << "] with [" << predicate << "]" << endl;
     struct hyperdex_client_attribute_check check;
     check.attr = attr;
     check.value = value;
@@ -117,33 +125,49 @@ search(const char* attr, const char* value, hyperpredicate predicate)
     int counter = 0;
     while (status != HYPERDEX_CLIENT_SEARCHDONE && status != HYPERDEX_CLIENT_NONEPENDING)
     {
-        if (verbose) cout << "(" << ++counter << ")" << endl;
+        cout << "(" << ++counter << ")" << endl;
         print_return();
         read();
     }
-    if (verbose) cout << endl;
+    cout << "\nSearch finished successfully" << endl;
 }
 
 int
 main(int argc, const char* argv[])
 {
-    //hyperdex::connect_opts conn;
-    //h = new hyperdex::Client(conn.host(), conn.port());
-    h = new hyperdex::Client("127.0.0.1", 1982);
-    
-    if (argc > 1)
+    e::argparser ap;
+    ap.autohelp();
+    ap.arg().name('p', "port")
+        .description("port on the hyperdex coordinator")
+        .metavar("p")
+        .as_long(&_hyper_port);
+    ap.arg().name('h', "host")
+        .description("address of hyperdex coordinator")
+        .metavar("p")
+        .as_string(&_hyper_host);
+    ap.arg().name('q', "query")
+        .description("file name regex")
+        .metavar("q")
+        .as_string(&_query);
+    ap.arg().name('v', "verbose")
+        .description("verbose outputs")
+        .metavar("v")
+        .set_true(&_verbose);
+
+    if (!ap.parse(argc, argv))
     {
-        if (argc > 2) verbose = true;
-
-        string query("^");
-        query += string(argv[1]);
-
-        search("path", query.c_str(), HYPERPREDICATE_REGEX);
+        return EXIT_FAILURE;
     }
     else
     {
-        cout << "Usage: ./wtf-stat <file_name_regex>" << endl;
-        cout << "Set up: echo 'space wtf key path attributes string blockmap, int directory, int mode' | hyperdex add-space -h 127.0.0.1 -p 1982" << endl;
+        h = new hyperdex::Client(_hyper_host, _hyper_port);
+        string query("^");
+        query += string(_query);
+        search("path", query.c_str(), HYPERPREDICATE_REGEX);
+
+        //cout << "Usage: wtf-stat <file_name_regex>" << endl;
+        //cout << "Type wtf-stat --help to see options" << endl;
+        //cout << "Set up: echo 'space wtf key path attributes string blockmap, int directory, int mode' | hyperdex add-space -h 127.0.0.1 -p 1982" << endl;
     }
 
     return EXIT_SUCCESS;
