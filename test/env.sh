a7296505300d025f90aea3364b7a7df4cab66a39
Sean Ogden <positivelyskewed.sf@gmail.com>
2014-04-08 14:13:03 -0400

    fix distcheck.
    Signed-off-by: Sean Ogden <positivelyskewed.sf@gmail.com>

--- test/env.sh
+++ test/env.sh
@@ -2,4 +2,4 @@ export WTF_SRCDIR="$1"
 export WTF_BUILDDIR="$2"
 export WTF_VERSION="$3"
 
-export CLASSPATH="${WTF_BUILDDIR}"/bindings/java/org.wtf.client-${WTF_VERSION}.jar:"${WTF_BUILDDIR}"/test/java:
+export CLASSPATH="${WTF_SRCDIR}"/bindings/java/org.wtf.client-${WTF_VERSION}.jar:"${WTF_BUILDDIR}"/test/java:
