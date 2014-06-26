2fe4a60ac94c41f1148f9f2a9db01d8f40e7d9ec
Sean Ogden <positivelyskewed.sf@gmail.com>
2014-06-26 11:46:16 -0400

    add back jianneng's backup code.

--- test/sh/readwrite_sync_stress_test.basic.sh
+++ test/sh/readwrite_sync_stress_test.basic.sh
@@ -2,6 +2,6 @@
 exec python "${WTF_SRCDIR}"/test/runner.py --wtf-daemons=2 --hyperdex-daemons=1 -- \
      "${WTF_BUILDDIR}"/test/readwrite-sync-stress-test -h {WTF_HOST} -p {WTF_PORT} \
       -H {HYPERDEX_HOST} -P {HYPERDEX_PORT} --file-length=64 --file-charset='hex' \
-      --value-length=100000 -n 1 -b 10 --min-read-length=1 --max-read-length=100000 \
+      --value-length=100000 -n 10 -b 1000 --min-read-length=1 --max-read-length=100000 \
       --min-write-length=1 --max-write-length=100000
      #libtool --mode=execute gdbtui --args \
